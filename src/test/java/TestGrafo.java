4645718511cf35e69f354e4850300e7dcf91dd60
Danii <danielfernandezlopez75@gmail.com>
2022-05-26 17:11:42 +0200

    Desarrollo 2.3 Test2

--- src/test/java/TestGrafo.java
+++ src/test/java/TestGrafo.java
@@ -33,15 +33,40 @@ public class TestGrafo {
         expectedPath.add(5);
         expectedPath.add(4);
 
-        List<Integer> expectedPath2 = new ArrayList<>();
-        expectedPath2.add(3);
-
-        expectedPath2.add(2);
 //We check if the returned path is equal to the expected one.
         assertEquals(expectedPath, g.shortestPath(1, 4));
-        assertEquals(expectedPath2, g.shortestPath(3, 2));
         System.out.println("Expected path: " + expectedPath + "\n" + "Returned path: " + g.shortestPath(1, 4));
-        System.out.println("Expected path: " + expectedPath2 + "\n" + "Returned path: " + g.shortestPath(3, 2));
+        System.out.println("Test shortestPathFindsAPath passed");
+    }
+
+    @Test
+    public void shortestPathFindsAPath2() {
+        System.out.println("\nTest shortestPathFindsAPath");
+        System.out.println("----------------------------");
+// We build the graph
+        Grafo<Integer> g2 = new Grafo();
+        g2.addVertex(1);
+        g2.addVertex(2);
+        g2.addVertex(3);
+        g2.addVertex(4);
+        g2.addVertex(5);
+
+        g2.addEdge(1, 2);
+        g2.addEdge(1, 5);
+        g2.addEdge(2, 3);
+        g2.addEdge(3, 4);
+        g2.addEdge(4, 5);
+        g2.addEdge(5, 4);
+        g2.addEdge(5, 2);
+
+// We build the expected path
+        List<Integer> expectedPath2 = new ArrayList<>();
+        expectedPath2.add(3);
+        expectedPath2.add(2);
+        expectedPath2.add(5);
+//We check if the returned path is equal to the expected one.
+        assertEquals(expectedPath2, g2.shortestPath(3,5));
+        System.out.println("Expected path: " + expectedPath2 + "\n" + "Returned path: " + g2.shortestPath(3, 5));
         System.out.println("Test shortestPathFindsAPath passed");
     }
 }
